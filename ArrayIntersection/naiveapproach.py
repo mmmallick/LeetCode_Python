30a4b2f03f0cc33efa4be19d4748bc6fd84b918d
Muhammad Mehmood Mallick <mmallick@furman.edu>
2020-09-22 20:52:33 -0400

    Create naiveapproach.py

---
+++ ArrayIntersection/naiveapproach.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums3=[]
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if(nums1[i]==nums2[j]):
+                    temp=nums2[j]
+                    nums2[j]=None
+                    nums3.append(temp)
+                    break
+        return nums3
+                    
